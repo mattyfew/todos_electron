2f5445595cd2555f8a67a66bab1bed9b5af16b40
Matt Fewer <mattfewer@hotmail.com>
2019-02-20 21:22:27 +0100

    first commit, debugged menu issue

---
+++ index.js
@@ -0,0 +1,48 @@
+const electron = require('electron')
+
+const {
+    app,
+    BrowserWindow,
+    Menu
+} = electron
+let mainWindow
+let appWindow
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({})
+    mainWindow.loadURL(`file://${__dirname}/main.html`)
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate)
+    Menu.setApplicationMenu(mainMenu)
+})
+
+function createAddWindow() {
+    addWindow = new BrowserWindow({
+        width: 300,
+        height: 200,
+        title: 'Add New Todo'
+    });
+
+}
+
+const menuTemplate = [{
+    label: 'File',
+    submenu: [{
+            label: 'New Todo',
+            click() {
+                createAddWindow();
+            }
+        },
+        {
+            label: 'Quit',
+            accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+            click() {
+                app.quit();
+            }
+        }
+    ]
+}]
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({ label: '' })
+}
