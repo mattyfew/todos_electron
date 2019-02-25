95ed2b4a2745f08e95ca37e1bb88f553dac088a2
Matt Fewer <mattfewer@hotmail.com>
2019-02-25 21:44:41 +0100

    project complete

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const electron = require('electron')
 const {
     app,
     BrowserWindow,
-    Menu
+    Menu,
+    ipcMain
 } = electron
 let mainWindow
 let appWindow
@@ -11,6 +12,7 @@ let appWindow
 app.on('ready', () => {
     mainWindow = new BrowserWindow({})
     mainWindow.loadURL(`file://${__dirname}/main.html`)
+    mainWindow.on('closed', () => app.quit())
 
     const mainMenu = Menu.buildFromTemplate(menuTemplate)
     Menu.setApplicationMenu(mainMenu)
@@ -22,22 +24,34 @@ function createAddWindow() {
         height: 200,
         title: 'Add New Todo'
     });
-
+    addWindow.loadURL(`file://${__dirname}/add.html`)
+    addWindow.on('closed', () => addWindow = null)
 }
+
+ipcMain.on('todo:add', (event, todo) => {
+    mainWindow.webContents.send('todo:add', todo)
+    addWindow.close()
+})
 
 const menuTemplate = [{
     label: 'File',
     submenu: [{
             label: 'New Todo',
             click() {
-                createAddWindow();
+                createAddWindow()
+            }
+        },
+        {
+            label: 'Clear',
+            click() {
+                mainWindow.webContents.send('todo:clear')
             }
         },
         {
             label: 'Quit',
             accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
             click() {
-                app.quit();
+                app.quit()
             }
         }
     ]
@@ -45,4 +59,20 @@ const menuTemplate = [{
 
 if (process.platform === 'darwin') {
     menuTemplate.unshift({ label: '' })
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    menuTemplate.push({
+        label: 'Developer',
+        submenu: [
+            { role: 'reload' },
+            {
+                label: 'Toggle Developer Tools',
+                accelerator: process.platform === 'darwin' ? 'Command+Alt+I' : 'Ctrl+Shift+I',
+                click(item, focusedWindow) {
+                    focusedWindow.toggleDevTools()
+                }
+            }
+        ]
+    })
 }
